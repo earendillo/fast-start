71a32128162e8bb2c7d762025218ac87727c3c6c
magmat88 <magdalena.matusiak88@gmail.com>
2021-11-22 23:25:34 +0100

    changed comment

--- src/app/components/articles.component.tsx
+++ src/app/components/articles.component.tsx
@@ -9,7 +9,7 @@ export function Articles() {
         <div>
             <h2>Articles</h2>
             {articles &&
-            //props && props.children && props.children.value props?.children?.value
+            //props && props.children && props.children.value instead props?.children?.value
                 articles.articles?.map((article: Article, index: number) => (
                     <div key={`${index}_${article.newsSite}_${article.id}`}>
                         <p>{article.id}</p>
