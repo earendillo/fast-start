fd0e1eafcb4462788c1532a3b29f996d4740e15a
Mateusz Matusiak <mateusz.matusiak@ardentcode.com>
2021-12-02 21:02:28 +0100

    work in progress - introduced contentTile component

--- src/app/components/mainContent/mainContent.component.tsx
+++ src/app/components/mainContent/mainContent.component.tsx
@@ -1,64 +1,23 @@
 import { useStoreon } from 'storeon/react';
-import { Article, Blog, Report } from '../../modules/content/content.state';
+import { ContentItem, Report } from '../../modules/content/content.state';
 import './mainContent.component.scss';
+import { ContentTile } from '../contentItem/contentTile.component';
 
-export function MainContent(): JSX.Element {
+export function MainContent(): JSX.Element | null {
     const { content } = useStoreon('content');
 
+    if (!content) return null;
     return (
-        <div className="main-content-container">
+        content && <div className="main-content-container">
             <div className="articles-and-blogs-container">
                 <div className="articles-container">
-                    {content &&
-                        content.articles
-                            ?.slice(0, 1)
-                            .map((article: Article, index: number) => (
-                                <div
-                                    className="article-item"
-                                    key={`${index}_${article.newsSite}_${article.id}`}
-                                >
-                                    <div className="article-item-img-and-title">
-                                        <div className="article-item-img">
-                                            <img
-                                                alt="article pic"
-                                                src={`${article.imageUrl}`}
-                                            />
-                                        </div>
-                                        <div className="article-item-title">
-                                            <p>{article.newsSite}</p>
-                                        </div>
-                                    </div>
-                                    <div className="article-item-summary">
-                                        <p>{article.summary}</p>
-                                    </div>
-                                </div>
-                            ))}
+                    {content?.articles[0] && <ContentTile contentItem={content.articles[0]} />}
                 </div>
                 <div className="blogs-container">
-                    {content &&
+                    {content?.blogs?.length >= 2 &&
                         content.blogs
                             ?.slice(0, 2)
-                            .map((blog: Blog, index: number) => (
-                                <div
-                                    className="blog-item"
-                                    key={`${index}_${blog.newsSite}_${blog.id}`}
-                                >
-                                    <div className="blog-item-img-and-title">
-                                        <div className="blog-item-img">
-                                            <img
-                                                alt="blog pic"
-                                                src={`${blog.imageUrl}`}
-                                            />
-                                        </div>
-                                        <div className="blog-item-title">
-                                            <p>{blog.newsSite}</p>
-                                        </div>
-                                    </div>
-                                    <div className="blog-item-summary">
-                                        <p>{blog.summary}</p>
-                                    </div>
-                                </div>
-                            ))}
+                            .map((blog: ContentItem) => <ContentTile contentItem={blog} />)}
                 </div>
             </div>
             <div className="reports-container">
