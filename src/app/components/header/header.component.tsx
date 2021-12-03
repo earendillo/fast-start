43cbcdabb68be96ba6a9622e031566e188c46708
magmat88 <magdalena.matusiak88@gmail.com>
2021-12-03 14:16:56 +0100

    Adjusted responsive display with responsive menu

--- src/app/components/header/header.component.tsx
+++ src/app/components/header/header.component.tsx
@@ -7,36 +7,38 @@ interface HeaderProps {
 
 function NavBar(props: any) {
     return (
-        <div className="navbar-container">
-            <nav className="navigation-container">
-                <ul className="navigation-list">
-                    <li className="navigation-list-item">
-                        <Link href={'/'}>Home</Link>
-                    </li>
-                    <li className="navigation-list-item">
-                        <Link href={'/articles'}>Articles</Link>
-                    </li>
-                    <li className="navigation-list-item">
-                        <Link href={'/reports'}>Reports</Link>
-                    </li>
-                    <li className="navigation-list-item">
-                        <Link href={'/blogs'}>Blogs</Link>
-                    </li>
-                    <li className="navigation-list-item">
-                        <Link href={'/about-page'}>About page</Link>
-                    </li>
-                </ul>
-            </nav>
-            <main>{props.children || null}</main>
-        </div>
+        <nav className="navigation-container">
+            <ul className="navigation-list">
+                <li className="navigation-list-item">
+                    <Link href={'/'}>Home</Link>
+                </li>
+                <li className="navigation-list-item">
+                    <Link href={'/articles'}>Articles</Link>
+                </li>
+                <li className="navigation-list-item">
+                    <Link href={'/reports'}>Reports</Link>
+                </li>
+                <li className="navigation-list-item">
+                    <Link href={'/blogs'}>Blogs</Link>
+                </li>
+                <li className="navigation-list-item">
+                    <Link href={'/about-page'}>About page</Link>
+                </li>
+            </ul>
+        </nav>
     );
 }
 
 export function MainHeader({ appName }: HeaderProps) {
     return (
         <div className="header-container">
-            <div className="product-name-container">
-                <p>{ appName }</p>
+            <div className="header-logo-and-icon-container">
+                <div className="product-name-container">
+                    <p>{ appName }</p>
+                </div>
+                <div className="hamburger-icon-container">
+                    <button>≡×</button>
+                </div>
             </div>
             <NavBar />
         </div>
