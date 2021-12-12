82693b0ab5a19b81606c9738a48ca36d5aeb42f4
magmat88 <magdalena.matusiak88@gmail.com>
2021-12-12 11:41:38 +0100

    Added styles to articles, blogs, reports and Added styles to articles, blogs, reports and about page components.

--- src/app/components/aboutPage/aboutPage.component.tsx
+++ src/app/components/aboutPage/aboutPage.component.tsx
@@ -2,7 +2,7 @@ import './aboutPage.component.scss';
 
 export function AboutPage() {
     return (
-        <div>
+        <div className="about-page-component">
             <h2>About page</h2>
             'about page content - change me'
         </div>
