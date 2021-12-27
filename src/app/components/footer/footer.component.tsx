30a8b837677f616b19cb72a461e577001cdfc8af
magmat88 <magdalena.matusiak88@gmail.com>
2021-12-27 15:01:55 +0100

    Added link to used API in footer.

--- src/app/components/footer/footer.component.tsx
+++ src/app/components/footer/footer.component.tsx
@@ -2,14 +2,27 @@ import './footer.component.scss';
 
 interface FooterProps {
     aboutPageInfo: string;
-    usedAPI: string;
 }
 
-export function Footer({ aboutPageInfo, usedAPI }: FooterProps) {
+export function Footer({ aboutPageInfo }: FooterProps): JSX.Element {
+    function renderAPIInfo(): JSX.Element {
+        return (
+            <p>
+                This page relies on data provided by
+                <a
+                    href="https://api.spaceflightnewsapi.net/v3/documentation"
+                    target="_blank"
+                >
+                     Spaceflight News API
+                </a>
+            </p>
+        );
+    }
+
     return (
         <div className="footer-container">
             <div className="info-container">{aboutPageInfo}</div>
-            <div className="used-api-container">{usedAPI}</div>
+            <div className="used-api-container">{renderAPIInfo()}</div>
         </div>
     );
 }
