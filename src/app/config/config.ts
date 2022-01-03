ab41c561f9635d3e19f4a303068cfe73c32ed6f3
magmat88 <magdalena.matusiak88@gmail.com>
2022-01-03 23:02:17 +0100

    Created file structure for config store. Created config file with static content for about page.

---
+++ src/app/config/config.ts
@@ -0,0 +1,120 @@
+export const config = {
+    aboutPage: {
+        facts: {
+            headerContent: 'Some reasons to keep on track with spaceflights',
+            data: [
+                {
+                    iconName: 'fact',
+                    iconClassName: 'icon-small',
+                    alt: 'Light bulb icon',
+                    iconSource:
+                        'https://pixabay.com/pl/vectors/%c5%bcar%c3%b3wka-pomys%c5%82-roz%c5%bcarzony-lampa-1294335/',
+                    href: 'https://www.nasa.gov/feature/facts-and-figures',
+                    content: [
+                        'There were over 140 spaceflight launches last year',
+                        'There are at least 7 people on space station right now',
+                        'In 24 hours, the space station is travelling through 16 sunrises and sunsets',
+                        'The space for living and working on space station is larger than a six-bedroom house',
+                    ],
+                },
+            ],
+        },
+        contentTypes: {
+            headerContent: 'We provide fresh content',
+            data: [
+                {
+                    iconName: 'news',
+                    iconClassName: 'icon-small',
+                    alt: 'Newspaper icon',
+                    iconSource:
+                        'https://pixabay.com/pl/vectors/gazeta-czyta%c4%87-aktualno%c5%9bci-biznes-311272/',
+                    content: 'Latest spaceflight news',
+                },
+                {
+                    iconName: 'blog',
+                    iconClassName: 'icon-small',
+                    alt: 'Sheet of paper with fountain pen icon',
+                    iconSource:
+                        'https://pixabay.com/pl/vectors/pisa%c4%87-list-poczta-d%c5%82ugopis-tekst-1292838/',
+                    content: 'Detailed blogs about missions and launches',
+                },
+                {
+                    iconName: 'reports',
+                    iconClassName: 'icon-medium',
+                    alt: 'Graph on monitor icon',
+                    iconSource:
+                        'https://pixabay.com/pl/vectors/p%c5%82aski-projekt-symbol-ikona-www-2126876/',
+                    content: 'Latest data from space stations and missions',
+                },
+            ],
+        },
+        missionAndValues: {
+            headerContent: 'Our mission and core values',
+            data: [
+                {
+                    iconName: 'quality',
+                    iconClassName: 'icon-medium',
+                    alt: 'Order of quality icon',
+                    iconSource:
+                        'https://pixabay.com/pl/vectors/nagroda-medal-gwiazda-jako%c5%9b%c4%87-5572089/',
+                    content: 'To deliver high quality product',
+                },
+                {
+                    iconName: 'interesting',
+                    iconClassName: 'icon-small',
+                    alt: 'Hand with thumb up icon',
+                    iconSource:
+                        'https://pixabay.com/pl/vectors/r%c4%99ka-palec-kciuk-kciuk-w-g%c3%b3r%c4%99-159474/',
+                    content: 'To deliver interesting content',
+                },
+                {
+                    iconName: 'tracking',
+                    iconClassName: 'icon-small',
+                    alt: 'Magnifier glass icon',
+                    iconSource:
+                        'https://pixabay.com/pl/vectors/szukaj-znajdowa%c4%87-lupa-powi%c4%99kszenie-6699087/',
+                    content: 'To make tracking spaceflight information easier',
+                },
+                {
+                    iconName: 'programmer',
+                    iconClassName: 'icon-medium-large',
+                    alt: 'Programmer working at the computer icon',
+                    iconSource:
+                        'https://pixabay.com/pl/vectors/facet-programista-komputer-post%c4%99p-6070329/',
+                    content: 'To develop our programming skills',
+                },
+                {
+                    iconName: 'heart',
+                    iconClassName: 'icon-small',
+                    alt: 'Heart icon',
+                    iconSource:
+                        'https://pixabay.com/pl/vectors/czarny-serce-mi%c5%82o%c5%9b%c4%87-czarna-mi%c5%82o%c5%9b%c4%87-152877/',
+                    content: 'To do what we love, to love what we do',
+                },
+            ],
+        },
+        team: {
+            headerContent: 'Our team',
+            data: [
+                {
+                    iconName: 'teamMember',
+                    iconClassName: 'icon-large',
+                    alt: 'Team member icon',
+                    iconSource:
+                        'https://pixabay.com/pl/vectors/u%c5%bcytkownik-osoba-ludzie-profil-1633249/',
+                    href: 'https://github.com/magmat88',
+                    content: 'Magda',
+                },
+                {
+                    iconName: 'teamMember',
+                    iconClassName: 'icon-large',
+                    alt: 'Team member icon',
+                    iconSource:
+                        'https://pixabay.com/pl/vectors/u%c5%bcytkownik-osoba-ludzie-profil-1633249/',
+                    href: 'https://github.com/earendillo',
+                    content: 'Mateusz',
+                },
+            ],
+        },
+    },
+};
