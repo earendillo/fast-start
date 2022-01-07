12307f86ac18702edd2e9abd59eb3e0b74843f4e
magmat88 <magdalena.matusiak88@gmail.com>
2022-01-07 13:08:58 +0100

    Fixed problem with text moving on tile hover

--- src/app/components/contentTile/contentTile.component.tsx
+++ src/app/components/contentTile/contentTile.component.tsx
@@ -14,33 +14,41 @@ export function ContentTile({ item, label, length }: ContentTileProps) {
 
     return (
         <section className="tile" onClick={handleClick}>
-            <div className="tile-img-container">
-                <img
-                    className="tile-img"
-                    alt="Illustration of current content"
-                    src={`${item.imageUrl}`}
-                />
-                <div
-                    className={`label-${ContentLabel[
-                        label
-                    ].toLowerCase()}`}
-                >
-                    {ContentLabel[label]}
-                </div>
-            </div>
-            <div className="tile-text-container">
-                <div className="summary">
-                        <h3 className={`tile-title-${ContentLabel[label].toLowerCase()}`}>
-                            {item.title}
-                        </h3>
-                    <p>{trimSummary(item.summary, length)}</p>
-                </div>
-                <div className="newssite-and-date">
-                    <span>{`${item.newsSite}, ${
-                        item.updatedAt
-                            ? trimDate(item.updatedAt)
-                            : trimDate(item.publishedAt)
-                    }`}</span>
+            <div className="tile-border">
+                <div className="tile-items">
+                    <div className="tile-img-container">
+                        <img
+                            className="tile-img"
+                            alt="Illustration of current content"
+                            src={`${item.imageUrl}`}
+                        />
+                        <div
+                            className={`label-${ContentLabel[
+                                label
+                            ].toLowerCase()}`}
+                        >
+                            {ContentLabel[label]}
+                        </div>
+                    </div>
+                    <div className="tile-text-container">
+                        <div className="summary">
+                            <h3
+                                className={`tile-title-${ContentLabel[
+                                    label
+                                ].toLowerCase()}`}
+                            >
+                                {item.title}
+                            </h3>
+                            <p>{trimSummary(item.summary, length)}</p>
+                        </div>
+                        <div className="newssite-and-date">
+                            <span>{`${item.newsSite}, ${
+                                item.updatedAt
+                                    ? trimDate(item.updatedAt)
+                                    : trimDate(item.publishedAt)
+                            }`}</span>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
