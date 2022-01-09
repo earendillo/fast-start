7f3ae0168fbbf71c136953bb7d7d15c63edfaf6c
Mateusz Matusiak <mateusz.matusiak@ardentcode.com>
2022-01-09 22:17:09 +0100

    fixed server to run on heroku

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -7,11 +7,7 @@ A
 public/robots.txt,d\a\dac799070c2d0c2fa24462b02c1f2dcc3fb3a1e9
 D
 public/manifest.json,4\2\42e89cd2213b9d5dfae4f63522702c623170d792
-?
-src/app/app.tsx,9\b\9b9c4e8b97a71282a0cae3ca9d75c75e9c4940d0
 ^
 .src/app/components/header/header.component.tsx,e\2\e21417bdaf255c9b379ec57d4275bc40c86f3e25
 D
 src/app/app.store.ts,d\a\da300295deac7a1c84bcb7dc4f20ef17c78647b9
-=
-src/index.tsx,5\a\5ab44fe33ca96299f51ad4778a73e425b92bf439
--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const ARTICLES_ROUTE = '/articles';
 const REPORTS_ROUTE = '/reports';
 const BLOGS_ROUTE = '/blogs';
 const DASHBOARD_ROUTE = '/dashboard';
+const ABOUTPAGE_ROUTE = '/about-page';
 
 fastify.register(require('fastify-static'), {
     root: path.join(__dirname, '..', './build')
@@ -73,6 +74,14 @@ fastify.get(DASHBOARD_ROUTE, async (req, reply) => {
         console.error(error);
     }
 })
+
+fastify.get(ABOUTPAGE_ROUTE, (req, reply) => {
+    try {
+        reply.send();
+    } catch (error) {
+        console.error(error);
+    }
+});
 
 fastify.listen(process.env.PORT || 5000, '0.0.0.0', function (err, port) {
     if (err) {
